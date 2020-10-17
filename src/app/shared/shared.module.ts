f51a4e3a71dd20b8b8f577412e204255e55db22c
kozrkozr <kozr23@gmail.com>
2020-10-17 16:40:40 +0300

    chore: add flex layout module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import {MaterialModule} from '@app/shared/modules/material/material.module';
 
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MaterialModule],
-  exports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FlexLayoutModule],
+  exports: [CommonModule, MaterialModule, FlexLayoutModule],
 })
 export class SharedModule {}
